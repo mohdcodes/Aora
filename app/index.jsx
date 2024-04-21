85fdb0321381bd372dfa6631335bd8423f11a98e
ARBAAZcode <arbaazsid0786@gmail.com>
2024-04-21 22:44:03 +0530

    onboarding screen done

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,49 @@
 //Entry File
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">AORA!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue' }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center  items-center px-4   min-h-[85vh]  ">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{' '}
+              <Text className="text-secondary-200">Aora</Text>{' '}
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] -right-8 -bottom-2 absolute"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            where creativity mets innovation: embark on a jouney of limitleess
+            with Aora
+          </Text>
+          <CustomButton
+            title="Countinue with email"
+            handlePress={() => router.push('/sign-in')}
+            containerStyle="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
